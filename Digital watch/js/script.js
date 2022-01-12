e93adbb359f1638e60371f7817ea5457a8b69559
ST Studios <97368006+STstudiosDev@users.noreply.github.com>
2022-01-12 15:28:10 -0300

    Add files via upload

---
+++ Digital watch/js/script.js
@@ -0,0 +1,13 @@
+function atualizarTempo(){
+
+var display = document.querySelector('.display');
+
+var agora = new Date();
+
+var horario = agora.getHours() + ':' + agora.getMinutes() + ':' + agora.getSeconds()
+
+display.textContent = horario;
+
+}
+
+setInterval(atualizarTempo, 1000);
